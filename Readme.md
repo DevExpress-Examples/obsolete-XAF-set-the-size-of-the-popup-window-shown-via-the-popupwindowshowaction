29d133c0fe0a37b0a70a2f1a22d9564e04908ac6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# OBSOLETE - How to set the size of the popup window shown via the PopupWindowShowAction in WinForms
+
+
+<p><strong>==========================================</strong><br /><strong>This example is now obsolete. Instead, refer to the <a href="https://www.devexpress.com/Support/Center/p/E4208">How to: Adjust the size of pop up dialogs</a>  example.</strong><br /><strong>==========================================</strong><br />To accomplish this you can handle the CustomizeTemplate event of the PopupWindowShowAction or XafApplication classes and access the actual template to set its size as needed.</p>
+<p><strong>See Also:</strong><br /> <a href="https://www.devexpress.com/Support/Center/p/E244">How to Show a Window via an Action</a></p>
+
+<br/>
+
+
---
+++ VB/E523/WinSolution.Module.Win/ViewController1.vb
@@ -0,0 +1,23 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.ExpressApp.Win.Templates
+
+Namespace WinSolution.Module.Win
+	Partial Public Class ViewController1
+		Inherits ViewController
+		Public Sub New()
+			InitializeComponent()
+			RegisterActions(components)
+		End Sub
+		Private Sub popupWindowShowAction1_CustomizeTemplate(ByVal sender As Object, ByVal e As CustomizeTemplateEventArgs) Handles popupWindowShowAction1.CustomizeTemplate
+			CType(e.Template, PopupForm).Size = New System.Drawing.Size(200, 200)
+		End Sub
+		Private Sub popupWindowShowAction1_CustomizePopupWindowParams(ByVal sender As Object, ByVal e As CustomizePopupWindowParamsEventArgs) Handles popupWindowShowAction1.CustomizePopupWindowParams
+			Dim os As IObjectSpace = Application.CreateObjectSpace()
+			e.View = Application.CreateDetailView(os, os.CreateObject(Of DomainObject1)())
+		End Sub
+	End Class
+End Namespace
